d18e1dec756b113a1722844495489b2031091ae2
Egor Kirdyashkin <e.kirdiashkin@smileexpo.ru>
2016-11-11 18:03:22 +0300

    fix code style

--- src/js/source/modal.js
+++ src/js/source/modal.js
@@ -1,30 +1,30 @@
-/* Modal popup*/
+// Modal popup
+
 var cookie = document.cookie;
+var date = new Date();
 
-//Modal show timer
-//	if (cookie.indexOf('popclose=submited') === -1) {
-//		setTimeout(function () {
-//			$('#Modal').modal('show');
-//		}, 2000); //Time popUp
-//	}
+// Modal show timer
+if (cookie.indexOf('popclose=submited') === -1) {
+    setTimeout(function () {
+        $('#Modal').modal('show');
+    }, 2000); // Time popUp
+}
 
-$("#Modal .close").click(function () {
-	var date = new Date;
-	date.setDate(date.getDate() + 14);
-	document.cookie = 'popclose=submited; expires=' + date.toGMTString();
+$('#Modal .close').click(function () {
+    date.setDate(date.getDate() + 14);
+    document.cookie = 'popclose=submited; expires=' + date.toGMTString();
 });
 
-$("#Modal").click(function (data, handler) {
-	if (data.target == this) {
-		var date = new Date;
-		date.setDate(date.getDate() + 14);
-		document.cookie = 'popclose=submited; expires=' + date.toGMTString();
-	}
+$('#Modal').click(function (data, handler) {
+    if (data.target === this) {
+        date.setDate(date.getDate() + 14);
+        document.cookie = 'popclose=submited; expires=' + date.toGMTString();
+    }
 });
 
-//For form id
-//	$("#lottery-popup-form").on('beforeSubmit', function () {
-//		var date = new Date;
-//		date.setDate(date.getDate() + 365);
-//		document.cookie = 'popclose=submited; expires=' + date.toGMTString();
-//	});
+// For form id
+$('#lottery-popup-form').on('beforeSubmit', function () {
+    date.setDate(date.getDate() + 365);
+    document.cookie = 'popclose=submited; expires=' + date.toGMTString();
+});
+// End Modal popup script
