6aaf68019916b3bfe5adf679597210a55bf13dba
Lucas Kane <Lucas Kane>
2016-11-19 19:57:11 +0300

    debounce code style

--- src/js/source/debounce.js
+++ src/js/source/debounce.js
@@ -3,23 +3,30 @@
 // N milliseconds. If `immediate` is passed, trigger the function on the
 // leading edge, instead of the trailing.
 function debounce(func, wait, immediate) {
-	var timeout;
-	return function() {
-		var context = this, args = arguments;
-		var later = function() {
-			timeout = null;
-			if (!immediate) func.apply(context, args);
-		};
-		var callNow = immediate && !timeout;
-		clearTimeout(timeout);
-		timeout = setTimeout(later, wait);
-		if (callNow) func.apply(context, args);
-	};
-};
+    var timeout;
+    return function () {
+        var context = this;
+        var args = arguments;
 
-//HOW IT USE
-// var myEfficientFn = debounce(function() {
-// 	// All the taxing stuff you do
+        var later = function () {
+            timeout = null;
+            if (!immediate) {
+                func.apply(context, args);
+            }
+        };
+
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) {
+            func.apply(context, args);
+        }
+    };
+}
+
+// HOW IT USE
+// var myEfficientFn = debounce(function () {
+//     // All the taxing stuff you do
 // }, 250);
 //
 // window.addEventListener('resize', myEfficientFn);
