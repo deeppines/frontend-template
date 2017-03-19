d8af9ba91f8b9db68f1525b9deda8b0ebe77d9c4
Lucas Kane <Lucas Kane>
2017-03-19 13:57:55 +0300

    add listener functions

---
+++ src/js/source/resizeListener.js
@@ -0,0 +1,5 @@
+var resizeListener = debounce(function () {
+    // Do something
+}, 200);
+
+window.addEventListener('resize', resizeListener);
---
+++ src/js/source/scrollListener.js
@@ -0,0 +1,5 @@
+var scrollListener = debounce(function () {
+    // Do something
+}, 200);
+
+window.addEventListener('scroll', scrollListener);
