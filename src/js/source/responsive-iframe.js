50e339762e31e27b3416f271a7f4ea826dbf86a4
EgorKir <atheus92@gmail.com>
2017-05-06 16:49:27 +0300

    del old js function files

--- src/js/source/responsive-iframe.js
+++
@@ -1,7 +0,0 @@
-// Responsive iframe video
-
-function responsiveIframe(contentContainer) {
-    var videoWrapper = '<div class="embed-responsive embed-responsive-16by9"></div>';
-    contentContainer.find('iframe').wrap(videoWrapper);
-}
-// END Responsive iframe video
