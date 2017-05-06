40d4b661f82c472ab9f4aa6819e183d0f204ff83
EgorKir <atheus92@gmail.com>
2017-05-06 16:49:42 +0300

    add new js functions

---
+++ source/js/function/responsive-iframe.js
@@ -0,0 +1,7 @@
+// Responsive iframe video
+
+function responsiveIframe(contentContainer) {
+    var videoWrapper = '<div class="embed-responsive embed-responsive-16by9"></div>';
+    contentContainer.find('iframe').wrap(videoWrapper);
+}
+// END Responsive iframe video
