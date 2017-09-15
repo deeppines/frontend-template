cd2a45ef517e053424ca51094791571290a05b1e
EgorKir <atheus92@gmail.com>
2017-09-15 20:45:02 +0300

    del old files

--- source/js/functions/responsive-iframe.js
+++
@@ -1,9 +0,0 @@
-// Responsive iframe video
-// nowrap - url parametr. For example '//embed.smileexpo'
-function responsiveIframe(contentContainer, nowrap) {
-    var videoWrapper = '<div class="embed-responsive embed-responsive-16by9"></div>';
-    var nowrapIframe = nowrap;
-
-    contentContainer.find('iframe').not("[src ^= nowrap]").wrap(videoWrapper);
-}
-// END Responsive iframe video
