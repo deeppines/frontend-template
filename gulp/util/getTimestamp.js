a0cf05e4e9ab5d1e5c63752329879e0554527093
EgorKir <atheus92@gmail.com>
2017-09-19 19:15:22 +0300

    add util

---
+++ gulp/util/getTimestamp.js
@@ -0,0 +1,10 @@
+// Return timestamp
+function getTimestamp() {
+    var now = new Date();
+    var year = now.getFullYear();
+    var month = correctNumber(now.getMonth() + 1);
+    var day = correctNumber(now.getDate());
+    var hours = correctNumber(now.getHours());
+    var minutes = correctNumber(now.getMinutes());
+    return year + '-' + month + '-' + day + '-' + hours + minutes;
+};
