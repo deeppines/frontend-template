4fb83728c30b8ff8a0fbed997dca4741515837b7
Egor Kirdyashkin <e.kirdiashkin@smileexpo.ru>
2017-09-20 13:49:49 +0300

    add new task

---
+++ gulp/tasks/watch-modules.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const gulp = require('gulp');
+const $    = require('gulp-load-plugins')();
+
+module.exports = function (options) {
+    return function () {
+        var watcher = gulp.watch(options.path);
+
+        watcher.on('addDir', function(path, stats) {
+            var pathArray = path.split("\\");
+            var folderName = pathArray[pathArray.length-1];
+
+            return gulp.src(path + '/*')
+                .pipe($.file(folderName + '.pug', '//- ' + folderName + ' module'))
+                .pipe($.file(folderName + '.scss', '/* ' + folderName + ' style */'))
+                .pipe($.file(folderName + '.js', '// ' + folderName + ' JavaScript'))
+                .pipe(gulp.dest(path));
+        });
+    };
+}
--- gulpfile.js
+++ gulpfile.js
@@ -18,6 +18,10 @@ lazyRequireTask('clean', './gulp/tasks/clean', {
     dst: 'web'
 });
 
+lazyRequireTask('watch', './gulp/tasks/watch-modules', {
+    path: 'source/modules/*'
+});
+
 var path = {
     watch: {
         modules: 'source/modules/*'
@@ -27,27 +31,3 @@ var path = {
         file: 'source/'
     }
 };
-
-var options = {
-    watchModuls: {
-        read: false,
-        ignorePermissionErrors: true,
-        events: [
-            'addDir',
-            'unlinkDir'
-        ]
-    }
-};
-
-gulp.task('watch', function () {
-    $.watch([path.watch.modules], options.watchModuls, function (event, cb) {
-        var pathArray = event.path.split("\\");
-        var folderName = pathArray[pathArray.length-1];
-
-        return gulp.src(event.path+ '/*')
-                .pipe($.file(folderName + '.pug', '//- ' + folderName + ' module'))
-                .pipe($.file(folderName + '.scss', '/* ' + folderName + ' style */'))
-                .pipe($.file(folderName + '.js', '// ' + folderName + ' JavaScript'))
-                .pipe(gulp.dest(event.path));
-    });
-});
