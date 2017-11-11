d8cfd7cdf60e528f6f0f156cd13d7b2a7a2922bb
EgorKir <atheus92@gmail.com>
2017-11-11 20:40:27 +0300

    add new util

--- gulp/utils/getTimestamp.js
+++ gulp/utils/getTimestamp.js
@@ -1,12 +1,16 @@
 'use strict';
+import correctNumber from './correctNumber';
 
 // Return timestamp
-function getTimestamp() {
-    var now = new Date();
-    var year = now.getFullYear();
-    var month = correctNumber(now.getMonth() + 1);
-    var day = correctNumber(now.getDate());
-    var hours = correctNumber(now.getHours());
-    var minutes = correctNumber(now.getMinutes());
+const getTimestamp = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = correctNumber(now.getMonth() + 1);
+    const day = correctNumber(now.getDate());
+    const hours = correctNumber(now.getHours());
+    const minutes = correctNumber(now.getMinutes());
+
     return year + '-' + month + '-' + day + '-' + hours + minutes;
 };
+
+export default getTimestamp;
