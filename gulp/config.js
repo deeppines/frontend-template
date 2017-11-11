2431dee2fa887c54bd4ecd473b874b907ff247e9
EgorKir <atheus92@gmail.com>
2017-11-11 22:32:52 +0300

    add new file

--- gulp/config.js
+++ gulp/config.js
@@ -1,1 +1,19 @@
 'use strict';
+
+var config = {
+    // =============================
+    // ---- Add var path task ------
+    // =============================
+
+    path: {
+        del:     './gulp/tasks/del',
+        html:    './gulp/tasks/html',
+        modules: './gulp/tasks/watch-modules'
+    },
+
+    pug: {
+        pretty: '\t'
+    }
+}
+
+module.exports = config;
