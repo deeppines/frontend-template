6e563c2a2d1045463d6511663dad9fac33605139
EgorKir <atheus92@gmail.com>
2017-11-12 22:44:05 +0300

    update utils

--- gulp/utils/correctNumber.js
+++ gulp/utils/correctNumber.js
@@ -1,7 +1,5 @@
 'use strict';
 
-module.exports = function () {
-    return function correctNumber(number) {
-        return number < 10 ? '0' + number : number;
-    };
+module.exports = function correctNumber(number) {
+    return number < 10 ? '0' + number : number;
 };
--- gulp/utils/getTimestamp.js
+++ gulp/utils/getTimestamp.js
@@ -1,16 +1,14 @@
 'use strict';
 
-const correctNumber = require('correctNumber.js');
+const correctNumber = require('./correctNumber');
 
-module.exports = function () {
-    return function getDateTime() {
-        const now = new Date();
-        const year = now.getFullYear();
-        const month = correctNumber(now.getMonth() + 1);
-        const day = correctNumber(now.getDate());
-        const hours = correctNumber(now.getHours());
-        const minutes = correctNumber(now.getMinutes());
+module.exports = function getDateTime() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = correctNumber(now.getMonth() + 1);
+    const day = correctNumber(now.getDate());
+    const hours = correctNumber(now.getHours());
+    const minutes = correctNumber(now.getMinutes());
 
-        return year + '-' + month + '-' + day + '-' + hours + minutes;
-    };
+    return year + '-' + month + '-' + day + '-' + hours + minutes;
 };
