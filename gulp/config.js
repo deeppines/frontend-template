f474a112d8c27a96bf0926c4e3ce2a6ac2704ad1
EgorKir <atheus92@gmail.com>
2017-11-18 19:11:40 +0300

    update tasks and config

--- gulp/config.js
+++ gulp/config.js
@@ -1,4 +1,5 @@
 import errorHandler from './utils/errorHandler';
+import attrSorter from 'posthtml-attrs-sorter';
 
 export const path = {
     root: 'web',
@@ -12,3 +13,41 @@ export const plumberConfig = {
 export const pugConfig = {
     pretty: '\t'
 };
+
+export const prettifyConfig = {
+    'unformatted': ['pre', 'code', 'textarea'],
+    'extra_liners': ['head', 'body', '/html', 'header', 'main', 'footer'],
+    'indent_size': 4,
+    'indent_char': ' ',
+    'indent_with_tabs': false,
+    'eol': '\n',
+    'end-with-newline': true,
+    'preserve_newlines': true,
+    'indent-inner-html': true,
+    'brace_style': 'expand'
+};
+
+export const posthtmlConfig = {
+    plugins: [
+        attrSorter({
+            order: [
+                'class',
+                'id',
+                'name',
+                'data',
+                'ng',
+                'src',
+                'for',
+                'type',
+                'href',
+                'values',
+                'title',
+                'alt',
+                'role',
+                'aria',
+                'tabindex'
+            ]
+        })
+    ],
+    options: {}
+}
--- gulp/tasks/html.js
+++ gulp/tasks/html.js
@@ -1,16 +1,26 @@
 import gulp from 'gulp';
+import gulpIf from 'gulp-if';
 import plumber from 'gulp-plumber';
 import pug from 'gulp-pug';
+import prettify from 'gulp-prettify';
+import posthtml from 'gulp-posthtml';
+import {isDevelopment} from '../utils/env';
 import {
     path,
     plumberConfig,
-    pugConfig
+    pugConfig,
+    prettifyConfig,
+    posthtmlConfig
 } from '../config';
 
 const html = () => {
     return gulp.src(['**/*.pug', '!**/_*.pug'], {cwd: 'source/pages'})
         .pipe(plumber(plumberConfig))
         .pipe(pug(pugConfig))
+        .pipe(gulpIf(!isDevelopment,
+            prettify(prettifyConfig),
+            posthtml(posthtmlConfig)
+        ))
         .pipe(gulp.dest(path.root));
 };
 
