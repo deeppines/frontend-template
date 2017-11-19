658b9d0c15fad2f4290d47b8b2f72f412e017951
EgorKir <atheus92@gmail.com>
2017-11-19 14:07:10 +0300

    update files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -4,11 +4,13 @@ import html from './gulp/tasks/html';
 import css from './gulp/tasks/scss';
 import clear from './gulp/tasks/clear';
 import zip from './gulp/tasks/zip';
+import {assets} from './gulp/tasks/assets';
 
 // Debug tasks
 task('html', html);
 task('css', css);
 task('clear', clear);
+task('assets', assets);
 
 // Main tasks
 task('build', build);
