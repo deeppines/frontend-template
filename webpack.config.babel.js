bc63c7e5e5ce7150188183e8599ed9603161c00c
EgorKir <atheus92@gmail.com>
2017-11-25 15:59:20 +0300

    update webpack config

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -3,12 +3,16 @@ import webpack from 'webpack';
 
 import {isDevelopment} from './gulp/utils/env';
 
-const webpackConfig = {
+const outputFileName = '[name]-webpack.bundle.js';
+
+let options = {
     entry: './source/static/scripts/common.js',
     output: {
-        path: path.resolve(__dirname, 'web'),
-        filename: 'common-webpack.bundle.js'
+        filename: outputFileName,
+        path: path.resolve(__dirname, 'web')
     },
+
+    devtool: 'source-map',
 
     module: {
         loaders: [
@@ -17,21 +21,34 @@ const webpackConfig = {
                 loader: 'babel-loader'
             }
         ]
-    },
+    }
+};
 
-    plugins: [
+options.plugins = [
+
+];
+
+if (isDevelopment) {
+    options.plugins.push(
         new webpack.optimize.UglifyJsPlugin({
+            sourceMap: true,
             uglifyOptions: {
                 ie8: true,
                 ecma: 5
             },
             output: {
                 comments: false,
-                beautify: true
+                beautify: true,
+                bracketize: true,
+                quote_style: 1
             },
             compress: false
         }),
-    ]
-};
+    );
+} else {
+    options.plugins.push(
 
-export default webpackConfig;
+    );
+}
+
+export default options;
