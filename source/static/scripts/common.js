0e470adf97d4b291e8af8cae5ba39dc58a7cbf12
EgorKir <atheus92@gmail.com>
2017-11-25 14:52:44 +0300

    update common.js

--- source/static/scripts/common.js
+++ source/static/scripts/common.js
@@ -1,1 +1,6 @@
-'use strict'
+// ====================================
+// --------- Functions ----------------
+// ====================================
+
+import debounce from './functions/debounce';
+import responsiveIframe from './functions/responsive-iframe';
