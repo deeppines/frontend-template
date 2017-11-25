89d51beeb9c6742d2fb05ae33952eca0ab6ee7d2
EgorKir <atheus92@gmail.com>
2017-11-25 15:58:56 +0300

    update task

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -3,6 +3,7 @@ import {series, parallel} from 'gulp';
 import clean from './clean';
 import html from './html';
 import css from './scss';
+import scripts from './scripts';
 import zip from './zip';
 import {assets} from './assets';
 
@@ -12,7 +13,8 @@ const build = series(
         parallel(
             assets,
             html,
-            css
+            css,
+            scripts
         ),
         zip
     )
