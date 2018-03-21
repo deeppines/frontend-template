792df0dccbc7cc51ed8dd6f4a336cc11944a4138
Egor Kirdyashkin <e.kirdiashkin@smileexpo.ru>
2018-03-21 09:29:45 +0300

    small fix

--- source/js/functions/responsive-iframe.js
+++ source/js/functions/responsive-iframe.js
@@ -4,9 +4,7 @@
 /* eslint-disable no-unused-vars */
 function responsiveIframe(contentContainer, videoWrapper, nowrap) {
     'use strict';
-
     videoWrapper = videoWrapper || '<div class="embed-responsive embed-responsive-16by9"></div>';
-
     contentContainer.find('iframe').not('[src ^=' + nowrap + ']').wrap(videoWrapper);
 }
 // END Responsive iframe video
