00ec8c392943d7ad756dcbe6f00ac63b17eefd0c
Egor Kirdyashkin <e.kirdiashkin@smileexpo.ru>
2018-04-13 11:49:22 +0300

    update script

--- source/static/scripts/functions/responsive-iframe.js
+++ source/static/scripts/functions/responsive-iframe.js
@@ -1,10 +1,12 @@
 // Responsive iframe video
 // nowrap - url parametr. For example '//embed.smileexpo'
 
-/* eslint-disable no-unused-vars */
-function responsiveIframe(contentContainer, videoWrapper, nowrap) {
-    'use strict';
-    videoWrapper = videoWrapper || '<div class="embed-responsive embed-responsive-16by9"></div>';
-    contentContainer.find('iframe').not('[src ^=' + nowrap + ']').wrap(videoWrapper);
-}
+const responsiveIframe = (contentContainer, nowrap) => {
+    let videoWrapper = '<div class="embed-responsive embed-responsive-16by9"></div>';
+    let nowrapIframe = nowrap;
+
+    contentContainer.find('iframe').not(`[src ^= ${nowrapIframe}]`).wrap(videoWrapper);
+};
+
+export default responsiveIframe;
 // END Responsive iframe video
